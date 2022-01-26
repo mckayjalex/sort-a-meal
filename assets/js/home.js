b5b491577246dc7cee49794a195186b80b381e0d
Clay <claymcintosh@gmail.com>
2022-01-26 15:16:09 +1000

    general layout for home page working. Needs polishing.

--- assets/js/home.js
+++ assets/js/home.js
@@ -19,3 +19,44 @@ function getUserLocation() {
 
   navigator.geolocation.getCurrentPosition(success, error, options);
 }
+
+
+
+
+
+// var sort = document.getElementById("sortbtn");
+
+// sort.addEventListener("click", text());
+
+// function test() {
+
+//     console.log("clicked")
+// }
+
+// test = 'https://www.sortameal.com/categories.html/type/restaurant/Calzon'
+// https://www.sortameal.com/index.html
+// https://www.sortameal.com/home.html
+// https://www.sortameal.com/categories.html/type/recipe/rice
+
+
+// document.location.href.split('/').reverse()[0]
+
+
+
+// /categories.html?type:restaurant
+
+
+// categories.html ? type : recipe
+
+// var item;
+// if(categories.html == true){
+//     item = type;
+// } else {
+//     item = recipe;
+// }
+
+// let x;
+
+// if(x > 0) {
+//     return 
+// }
