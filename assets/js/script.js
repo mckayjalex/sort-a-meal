b560ffccc813403f951c6bef7f5ff85b79311741
Alex <chaza.mckay@me.com>
2022-01-26 22:56:20 +1030

    Added responsive design for mibile

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+function sendData() {
+    
+}
+
+
+
+
+
+// Category buttons
+
+// Burger button
+$('#burgers').on('click', function() {
+
+})
+// Indian button
+$('#indian').on('click', function() {
+  
+})
+// Asian button
+$('#asian').on('click', function() {
+  
+})
+// Pizza button
+$('#pizza').on('click', function() {
+
+})
+// Thai button
+$('#thai').on('click', function() {
+  
+})
+// Pasta button
+$('#pasta').on('click', function() {
+  
+})
