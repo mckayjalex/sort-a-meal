69ec995cf7ded001dd5b4a27e93547d277502b33
Alex <chaza.mckay@me.com>
2022-01-27 19:39:08 +1030

    Finalized and adde js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,40 @@
-function sendData() {
-    
+
+// Base URL
+let sortUrl = '/sort.html?';
+
+// Global variables
+let query;
+let coordinates;
+let type;
+
+// Called on load of page
+function init() {
+    getData();
 }
 
+function getData() {
+    let url = window.location.search;
+    let searchParems = new URLSearchParams(url);
+    coordinates = `coordinates=${searchParems.get('coordinates')}`;
+    type = `type=${searchParems.get('type')}`;
+}
 
+function getQuery(event) {
+    query = `q=${event.target.dataset.type}`;
+}
 
-
+function loadSortPage() {
+    if(coordinates) {
+        window.location = `${sortUrl}${coordinates}&${type}&${query}`;
+    } else {
+        window.location = `${sortUrl}${type}&${query}`;
+    }
+}
 
 // Category buttons
+$('.categories').on('click', function(event) {
+    getQuery(event);
+    loadSortPage();
+})
 
-// Burger button
-$('#burgers').on('click', function() {
-
-})
-// Indian button
-$('#indian').on('click', function() {
-  
-})
-// Asian button
-$('#asian').on('click', function() {
-  
-})
-// Pizza button
-$('#pizza').on('click', function() {
-
-})
-// Thai button
-$('#thai').on('click', function() {
-  
-})
-// Pasta button
-$('#pasta').on('click', function() {
-  
-})
+init();
