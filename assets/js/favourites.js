aaaab647a23c974978a910475c0976861f9af231
Alex <chaza.mckay@me.com>
2022-01-29 22:32:48 +1030

    Finalized with no saved data

--- assets/js/favourites.js
+++ assets/js/favourites.js
@@ -21,7 +21,7 @@ var sampleFavouritesObj = {
             image: {},
             name: "Boony's Restaurant and Pizzeria",
             vicinity: "1/32 Saltfleet St, Port Noarlunga",
-            ratings: 4.5,
+            ratings: 3.5,
             status: true,
             link: "ChIJ-RxcV1AhsWoRBX2oukgIEHQ",
         },
@@ -161,8 +161,8 @@ localStorage.setItem('favourites', JSON.stringify(sampleFavouritesObj));
 // Global variables
 let favourites;
 
-let restaurantTab = document.querySelector('#restaurants');
-let recipeTab = document.querySelector('#recipes');
+let restaurantBtn = document.querySelector('#restaurants');
+let recipeBtn = document.querySelector('#recipes');
 let restLine = document.querySelector('#restaurant-border');
 let recipeLine = document.querySelector('#recipe-border');
 let favContentRecipe = document.querySelector('#fav-content-recipe');
@@ -170,8 +170,8 @@ let favContentRestaurant = document.querySelector('#fav-content-restaurant');
 
 function init() {
     getData();
-    displayRestaurants(sampleFavouritesObj.restaurants);
-    displayRecipes(sampleFavouritesObj.recipes);
+    displayRestaurants(favourites.restaurants);
+    displayRecipes(favourites.recipes);
 }
 
 function restaurantButtonGraphics() {
@@ -195,13 +195,13 @@ function recipeButtonGraphics() {
     restaurantBtn.classList.add('text-gray-500');
 }
 // Restaurant tab button
-restaurantTab.addEventListener('click', function () {
+restaurantBtn.addEventListener('click', function () {
     favContentRecipe.classList.add('hidden');
     favContentRestaurant.classList.remove('hidden');
     restaurantButtonGraphics();
 })
 // Recipe tab button
-recipeTab.addEventListener('click', function () {
+recipeBtn.addEventListener('click', function () {
     favContentRestaurant.classList.add('hidden');
     favContentRecipe.classList.remove('hidden');
     recipeButtonGraphics();
@@ -209,6 +209,7 @@ recipeTab.addEventListener('click', function () {
 
 function getData() {
     favourites = JSON.parse(localStorage.getItem('favourites'));
+    // console.log(favourites);
 }
 
 // Displays all favourite restaurants
@@ -218,7 +219,7 @@ function displayRestaurants(restaurants) {
     for (let i = 0; i < restaurants.length; i++) {
         // Add container
         let container = document.createElement('div');
-        container.classList.add('font-boogaloo', 'h-52', 'sm:h-72', 'md:h-72', 'flex', 'flex-row', 'justify-center', 'rounded-2xl', 'shadow-xl', 'shadow-slate-500')
+        container.classList.add('h-52', 'sm:h-72', 'md:h-72', 'flex', 'flex-row', 'justify-center', 'rounded-2xl', 'shadow-xl', 'shadow-slate-500')
         favContentRestaurant.append(container);
         // Add image
         let image = document.createElement('img');
@@ -231,7 +232,7 @@ function displayRestaurants(restaurants) {
         container.append(textContainer);
         // Add name
         let name = document.createElement('h2');
-        name.classList.add('text-left', 'text-md', 'sm:text-3xl', 'md:text-3xl', 'lg:text-xl', 'xl:text-3xl');
+        name.classList.add('font-boogaloo', 'text-left', 'text-md', 'sm:text-3xl', 'md:text-3xl', 'lg:text-xl', 'xl:text-3xl');
         name.textContent = restaurants[i].name;
         textContainer.append(name);
         // Add address
@@ -242,7 +243,7 @@ function displayRestaurants(restaurants) {
         mapIcon.classList.add('fas', 'fa-map-marker-alt', 'pr-2');
         addressContainer.append(mapIcon);
         let address = document.createElement('p');
-        address.classList.add('text-sm', 'sm:text-lg', 'md:text-xl', 'lg:text-sm', 'xl:text-xl');
+        address.classList.add('font-boogaloo', 'text-sm', 'sm:text-lg', 'md:text-xl', 'lg:text-sm', 'xl:text-xl');
         address.textContent = restaurants[i].vicinity;
         addressContainer.append(address);
         // Add rating
@@ -250,7 +251,7 @@ function displayRestaurants(restaurants) {
         textContainer.append(ratingContainer);
         ratingContainer.classList.add('flex', 'justify-start', 'items-center');
         let rating = document.createElement('p');
-        rating.classList.add('text-sm', 'sm:text-lg', 'md:text-xl', 'px-2', 'lg:text-lg', 'xl:text-2xl');
+        rating.classList.add('font-boogaloo', 'text-sm', 'sm:text-lg', 'md:text-xl', 'px-2', 'lg:text-lg', 'xl:text-2xl');
         if (restaurants[i].ratings < 3) {
             rating.classList.add('rounded-lg', 'bg-red-500');
         } else if (restaurants[i].ratings > 3 && restaurants[i].ratings < 4) {
@@ -269,11 +270,13 @@ function displayRestaurants(restaurants) {
         textContainer.append(linkLikeContainer);
         linkLikeContainer.classList.add('flex', 'flex-row', 'w-full', 'items-center', 'justify-between');
         let link = document.createElement('a');
-        link.classList.add('text-sm', 'sm:text-xl', 'md:text-2xl', 'lg:text-lg', 'xl:text-xl', 'bg-slate-100', 'px-4', 'rounded-md', 'shadow-xl', 'shadow-slate-400', 'hover:bg-slate-300', 'cursor-pointer');
+        link.classList.add('font-boogaloo', 'text-sm', 'sm:text-xl', 'md:text-2xl', 'lg:text-lg', 'xl:text-xl', 'bg-slate-100', 'px-4', 'rounded-md', 'shadow-xl', 'shadow-slate-400', 'hover:bg-slate-300', 'cursor-pointer');
         link.textContent = 'More Info';
         link.setAttribute('href', restaurants[i].link);
+        link.setAttribute('target', '_blank');
         linkLikeContainer.append(link);
         let like = document.createElement('i');
+        like.setAttribute('data-index', i);
         like.classList.add('text-2xl', 'sm:text-3xl', 'md:text-5xl', 'lg:text-3xl', 'xl:text-5xl', 'fa', 'fa-heart', 'cursor-pointer', 'text-maingreen');
         linkLikeContainer.append(like);
     }
@@ -284,7 +287,7 @@ function displayRecipes(recipes) {
     for (let i = 0; i < recipes.length; i++) {
         // Add container
         let container = document.createElement('div');
-        container.classList.add('font-boogaloo', 'h-52', 'sm:h-72', 'md:h-72', 'flex', 'flex-row', 'justify-center', 'rounded-2xl', 'shadow-xl', 'shadow-slate-500')
+        container.classList.add('h-52', 'sm:h-72', 'md:h-72', 'flex', 'flex-row', 'justify-center', 'rounded-2xl', 'shadow-xl', 'shadow-slate-500')
         favContentRecipe.append(container);
         // Add image
         let image = document.createElement('img');
@@ -297,15 +300,32 @@ function displayRecipes(recipes) {
         container.append(textContainer);
         // Add title
         let title = document.createElement('h2');
-        title.classList.add('text-left', 'text-md', 'sm:text-3xl', 'md:text-3xl', 'lg:text-xl', 'xl:text-3xl');
+        title.classList.add('font-boogaloo', 'text-left', 'text-md', 'sm:text-3xl', 'md:text-3xl', 'lg:text-xl', 'xl:text-3xl');
         title.textContent = recipes[i].title;
         textContainer.append(title);
+        // Add Vegan status
+        let veganContainer = document.createElement('div');
+        textContainer.append(veganContainer);
+        veganContainer.classList.add('flex', 'justify-start', 'items-center', 'px-2', 'rounded');
+        let vegan = document.createElement('h2');
+        vegan.classList.add('font-boogaloo', 'text-md', 'sm:text-lg', 'md:text-xl', 'lg:text-xl', 'xl:text-xl');
+        vegan.textContent = 'Vegan';
+        veganContainer.append(vegan);
+        if (recipes[i].vegan) {
+            let veganIcon = document.createElement('i');
+            veganIcon.classList.add('fas', 'fa-check', 'pl-2', 'text-green-400', 'text-2xl');
+            veganContainer.append(veganIcon);
+        } else {
+            let veganIcon = document.createElement('i');
+            veganIcon.classList.add('fas', 'fa-times', 'pl-2', 'text-red-400', 'text-2xl');
+            veganContainer.append(veganIcon);
+        }
         // Add servings
         let servingsContainer = document.createElement('div');
         textContainer.append(servingsContainer);
         servingsContainer.classList.add('flex', 'justify-start', 'items-center', 'bg-offgrey', 'px-2', 'rounded');
         let servings = document.createElement('h2');
-        servings.classList.add('text-md', 'sm:text-lg', 'md:text-xl', 'lg:text-xl', 'xl:text-xl');
+        servings.classList.add('font-boogaloo', 'text-md', 'sm:text-lg', 'md:text-xl', 'lg:text-xl', 'xl:text-xl');
         servings.textContent = 'Servings: ' + recipes[i].servings;
         servingsContainer.append(servings);
         let servingsIcon = document.createElement('i');
@@ -316,7 +336,7 @@ function displayRecipes(recipes) {
         textContainer.append(ingredientsContainer);
         ingredientsContainer.classList.add('flex', 'justify-start', 'items-center');
         let ingredients = document.createElement('p');
-        ingredients.classList.add('text-sm', 'sm:text-lg', 'md:text-xl', 'lg:text-sm', 'xl:text-xl');
+        ingredients.classList.add('font-boogaloo', 'text-sm', 'sm:text-lg', 'md:text-xl', 'lg:text-sm', 'xl:text-xl');
         ingredients.textContent = 'Ingredients: ' + recipes[i].ingredients;
         ingredientsContainer.append(ingredients);
         let ingredientsIcon = document.createElement('i');
@@ -327,23 +347,40 @@ function displayRecipes(recipes) {
         textContainer.append(linkLikeContainer);
         linkLikeContainer.classList.add('flex', 'flex-row', 'w-full', 'items-center', 'justify-between');
         let link = document.createElement('a');
-        link.classList.add('text-sm', 'sm:text-xl', 'md:text-2xl', 'lg:text-lg', 'xl:text-xl', 'bg-slate-100', 'px-4', 'rounded-md', 'shadow-xl', 'shadow-slate-400', 'hover:bg-slate-300', 'cursor-pointer');
+        link.classList.add('font-boogaloo', 'text-sm', 'sm:text-xl', 'md:text-2xl', 'lg:text-lg', 'xl:text-xl', 'bg-slate-100', 'px-4', 'rounded-md', 'shadow-xl', 'shadow-slate-400', 'hover:bg-slate-300', 'cursor-pointer');
         link.textContent = 'More Info';
         link.setAttribute('href', recipes[i].link);
+        link.setAttribute('target', '_blank');
         linkLikeContainer.append(link);
         let like = document.createElement('i');
         like.classList.add('text-2xl', 'sm:text-3xl', 'md:text-5xl', 'lg:text-3xl', 'xl:text-5xl', 'fa', 'fa-heart', 'cursor-pointer', 'text-maingreen');
         linkLikeContainer.append(like);
     }
 }
+
+// function removeRestaurantFavourites(int) {
+//     favourites.restaurants.splice(int, 1);
+//     localStorage.setItem('favourites', JSON.stringify(favourites));
+//     console.log(favourites.restaurants);
+//     int--;
+// }
+// function removeRecipeFavourites(int) {
+//     favourites.recipes.splice(int, 1);
+//     localStorage.setItem('favourites', JSON.stringify(favourites));
+//     console.log(favourites.recipes);
+// }
+
 // Add link button animation
-favContentRecipe.addEventListener('click', function(event) {
+favContentRecipe.addEventListener('click', function (event) {
     event.target.classList.remove('fa')
     event.target.classList.add('far');
+    // removeRecipeFavourites(event.target.dataset.index);
 })
-favContentRestaurant.addEventListener('click', function(event) {
+favContentRestaurant.addEventListener('click', function (event) {
     event.target.classList.remove('fa')
     event.target.classList.add('far');
+    // removeRestaurantFavourites(event.target.dataset.index);
+
 })
 
 init();
